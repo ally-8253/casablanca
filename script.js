0bac3cf22b3d7b5ed7e5f54649fe01b503e66b36
Alisa <ally8252@gmail.com>
2019-08-02 12:10:29 +0600

    Add source failes

---
+++ script.js
@@ -0,0 +1,21 @@
+const anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href");
+
+    if (anchor.getAttribute("href") === "#") {
+      document.querySelector("body").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else {
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
+}
